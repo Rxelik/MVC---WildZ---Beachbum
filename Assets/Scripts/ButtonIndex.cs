3863738e17bd86057504aaf357789b532d1f5a56
Eli Zaidman <Rxelik5@gmail.com>
2022-09-26 14:51:25 +0300

    Turn Order

--- Assets/Scripts/ButtonIndex.cs
+++ Assets/Scripts/ButtonIndex.cs
@@ -12,28 +12,37 @@ public class ButtonIndex : MonoBehaviour
 
     public void ChoosePlayerCard(int index)
     {
-        if (playerModel.Cards[index].Color == deckModel.Cards[deckModel.Cards.Count - 1].Color
-           || playerModel.Cards[index].Number == deckModel.Cards[deckModel.Cards.Count - 1].Number)
+        if (boardModel.CurrentTurn == "Player")
         {
-            playerModel.Cards[index].Position = new Vector3(-5, 0, -5);
-            playerModel.Cards[index].Layer = deckModel.Cards[deckModel.Cards.Count - 1].Layer + 2;
-            deckModel.Cards.Add(playerModel.Cards[index]);
-            playerModel.Cards.Remove(playerModel.Cards[index]);
-            playerModel.HandCount--;
+            if (playerModel.Cards[index].Color == deckModel.Cards[deckModel.Cards.Count - 1].Color
+               || playerModel.Cards[index].Number == deckModel.Cards[deckModel.Cards.Count - 1].Number)
+            {
+                playerModel.Cards[index].Position = new Vector3(-5, 0, -5);
+                playerModel.Cards[index].Layer = deckModel.Cards[deckModel.Cards.Count - 1].Layer + 2;
+                deckModel.Cards.Add(playerModel.Cards[index]);
+                playerModel.Cards.Remove(playerModel.Cards[index]);
+                playerModel.HandCount--;
+                ChangeTurn();
+            }
         }
     }
 
     public void ChooseEnemyCard(int index)
     {
-        if (enemyModel.Cards[index].Color == deckModel.Cards[deckModel.Cards.Count - 1].Color
-           || enemyModel.Cards[index].Number == deckModel.Cards[deckModel.Cards.Count - 1].Number)
+        if (boardModel.CurrentTurn == "Enemy")
         {
-            enemyModel.Cards[index].Position = new Vector3(-5, 0, -5);
-            enemyModel.Cards[index].Layer = deckModel.Cards[deckModel.Cards.Count - 1].Layer + 2;
-            deckModel.Cards.Add(enemyModel.Cards[index]);
-            enemyModel.Cards.Remove(enemyModel.Cards[index]);
-            enemyModel.HandCount--;
+            if (enemyModel.Cards[index].Color == deckModel.Cards[deckModel.Cards.Count - 1].Color
+               || enemyModel.Cards[index].Number == deckModel.Cards[deckModel.Cards.Count - 1].Number)
+            {
+                enemyModel.Cards[index].Position = new Vector3(-5, 0, -5);
+                enemyModel.Cards[index].Layer = deckModel.Cards[deckModel.Cards.Count - 1].Layer + 2;
+                deckModel.Cards.Add(enemyModel.Cards[index]);
+                enemyModel.Cards.Remove(enemyModel.Cards[index]);
+                enemyModel.HandCount--;
+                ChangeTurn();
+            }
         }
+
     }
 
     public void TakeFromDeck()
@@ -43,13 +52,25 @@ public class ButtonIndex : MonoBehaviour
             playerModel.Cards.Add(boardModel.Cards[boardModel.Cards.Count - 1]);
             playerModel.HandCount--;
             boardModel.Cards.Remove(boardModel.Cards[boardModel.Cards.Count - 1]);
-            boardModel.CurrentTurn = "Enemy";
+            ChangeTurn();
         }
-        else if (boardModel.CurrentTurn == "Enemy")
+        else
         {
             enemyModel.Cards.Add(boardModel.Cards[boardModel.Cards.Count - 1]);
             enemyModel.HandCount--;
             boardModel.Cards.Remove(boardModel.Cards[boardModel.Cards.Count - 1]);
+            ChangeTurn();
+        }
+    }
+
+    public void ChangeTurn()
+    {
+        if (boardModel.CurrentTurn == "Player")
+        {
+            boardModel.CurrentTurn = "Enemy";
+        }
+        else
+        {
             boardModel.CurrentTurn = "Player";
         }
     }
