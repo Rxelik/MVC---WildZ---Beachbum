139ac58c8fe7deca48d1867b200b62d214332a62
YardenHR <yardenyhr@gmail.com>
2022-11-30 14:00:22 +0200

    <3
    <3

---
+++ Assets/TPM.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TPM : MonoBehaviour
+{
+   public TextMeshProUGUI childText;
+   public TextMeshProUGUI paranetText;
+    void Update()
+    {
+        childText.text = paranetText.text;
+    }
+}
