4a7eee3c2678a5cc08c0d26fc2579797a0f850a3
ELI-PC\rxeli <Zaidmaneli@gmail.com>
2022-12-20 16:16:45 +0200

    no message

--- Assets/Scripts/Manager/Inisializer.cs
+++ Assets/Scripts/Manager/Inisializer.cs
@@ -763,7 +763,7 @@ public class Inisializer : MonoBehaviour
         #endregion
 
         #region Bamoozle
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 2; i++)
         {
             var CardmodelFactory = new CardModelFactory();
             var _Cardmodel = CardmodelFactory.Model;
